73cacd90a69f940a348efbc21bc9566a3cef4463
Developer <ljk1291@googlemail.com>
2025-09-21 14:08:34 +0200

    Change rife

--- ccvfi/config/rife_config.py
+++ ccvfi/config/rife_config.py
@@ -14,7 +14,7 @@ RIFEConfigs = [
         name=ConfigType.RIFE_IFNet_v426_heavy,
         url="https://github.com/EutropicAI/ccvfi/releases/download/model_zoo/RIFE_IFNet_v426_heavy.pkl",
         hash="4cc518e172156ad6207b9c7a43364f518832d83a4325d484240493a9e2980537",
-        in_frame_count=2,
+        in_frame_count=6,
     )
 ]
 
